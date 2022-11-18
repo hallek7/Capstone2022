5046409d9f703658bd5b35190ae56a07eea25e4d
Halle <37225535+hallek7@users.noreply.github.com>
2022-11-18 10:13:23 -0500

    connection to the db

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+//const router = express.Router();
+const User = require('../models/User');
+
+
+//handle user creation 
+router.post('/')
+module.exports=router
+
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require ('cors');
+const userRoutes = require('./routes/userRoutes');
 
-app.use(cores());
+require('./connection');
+
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json({extended:true}));
-app.listen(5000,()=>{
+app.use('users', userRoutes);
+
+const port = app.listen(5000,()=>{
     console.log('Server is running on port');
 })
